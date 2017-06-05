4e862c6c367c4ac7a3a3161d3633a7bb9d6e7589
Simon Rozman <simon@rozman.si>
2017-06-05 09:00:29 +0200

    Initial commit

---
+++ eduJSONTests/ParserTests.cs
@@ -0,0 +1,62 @@
+﻿/*
+    Copyright 2017 Amebis
+
+    This file is part of eduJSON.
+
+    eduJSON is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    eduJSON is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with eduJSON. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace eduJSON.Tests
+{
+    [TestClass()]
+    public class ParserTests
+    {
+        [TestMethod()]
+        public void ParseTest()
+        {
+            Assert.AreEqual(true, Parser.Parse("// Test 1\n  true /* Trailing comment */"));
+            Assert.AreEqual(false, Parser.Parse("   false\r\n// Trailing comment"));
+            Assert.AreEqual(null, Parser.Parse("null"));
+            Assert.AreEqual(1234, Parser.Parse(" 1234 "));
+            Assert.AreEqual(1234, Parser.Parse(" +1234 "));
+            Assert.AreEqual(-1234, Parser.Parse(" -1234 "));
+            Assert.AreEqual(1.0870e-3, (double)Parser.Parse(" +1.0870e-3 "), 1e-10);
+            Assert.AreEqual(-2e+31, (double)Parser.Parse(" -2e+31 "), 1e-10);
+            Assert.AreEqual("This is a test.", Parser.Parse(" \"This is a test.\" "));
+            Assert.AreEqual("  \"/\\\b\f\n\r\t\u263a\\x  ", Parser.Parse(" \"  \\\"\\/\\\\\\b\\f\\n\\r\\t\\u263A\\x  \" "));
+
+            // CollectionAssert.AreEqual() does not work for sub-collections; Check manually.
+            object obj = Parser.Parse("{ \"key1\" : true , \"key2\" : [ 1, 2, 3, 4, 5], \"key3\" : { \"k1\": \"test1\", k2:\"test2\"}}");
+            Dictionary<string, object> obj_dict = (Dictionary<string, object>)obj;
+            Assert.AreEqual(3, obj_dict.Count);
+            Assert.AreEqual(true, obj_dict["key1"]);
+            CollectionAssert.AreEqual(new List<object>
+                {
+                    1,
+                    2,
+                    3,
+                    4,
+                    5
+                }, (List<object>)obj_dict["key2"]);
+            CollectionAssert.AreEqual(new Dictionary<string, object>
+                {
+                    { "k1", "test1" },
+                    { "k2", "test2" }
+                }, (Dictionary<string, object>)obj_dict["key3"]);
+        }
+    }
+}
