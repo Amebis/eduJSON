c9689ae2b8930966d57085370ad37d86a7bf0e2b
Simon Rozman <simon@rozman.si>
2017-06-05 12:20:26 +0200

    Test failure messages added

--- eduJSONTests/ParserTests.cs
+++ eduJSONTests/ParserTests.cs
@@ -28,22 +28,24 @@ namespace eduJSON.Tests
         [TestMethod()]
         public void ParseTest()
         {
-            Assert.AreEqual(true, Parser.Parse("// Test 1\n  true /* Trailing comment */"));
-            Assert.AreEqual(false, Parser.Parse("   false\r\n// Trailing comment"));
-            Assert.AreEqual(null, Parser.Parse("null"));
-            Assert.AreEqual(1234, Parser.Parse(" 1234 "));
-            Assert.AreEqual(1234, Parser.Parse(" +1234 "));
-            Assert.AreEqual(-1234, Parser.Parse(" -1234 "));
-            Assert.AreEqual(1.0870e-3, (double)Parser.Parse(" +1.0870e-3 "), 1e-10);
-            Assert.AreEqual(-2e+31, (double)Parser.Parse(" -2e+31 "), 1e-10);
-            Assert.AreEqual("This is a test.", Parser.Parse(" \"This is a test.\" "));
-            Assert.AreEqual("  \"/\\\b\f\n\r\t\u263a\\x  ", Parser.Parse(" \"  \\\"\\/\\\\\\b\\f\\n\\r\\t\\u263A\\x  \" "));
+            // Test basic data types.
+            Assert.AreEqual(true, Parser.Parse("// Test 1\n  True /* Trailing comment */"), "Boolean \"true\" not recognized");
+            Assert.AreEqual(false, Parser.Parse("   falSe\r\n// Trailing comment"), "Boolean \"false\" not recognized");
+            Assert.AreEqual(null, Parser.Parse("NULL"), "\"null\" not recognized");
+            Assert.AreEqual(1234, Parser.Parse(" 1234 "), "Integer not recognized");
+            Assert.AreEqual(1234, Parser.Parse(" +1234 "), "Integer with an explicit \"+\" sign not recognized (JSON EX)");
+            Assert.AreEqual(-1234, Parser.Parse(" -1234 "), "Negative integer not recognized");
+            Assert.AreEqual(1.0870e-3, (double)Parser.Parse(" +1.0870e-3 "), 1e-10, "Float not recognized");
+            Assert.AreEqual(-2e+31, (double)Parser.Parse(" -2e+31 "), 1e-10, "Negative float not recognized");
+            Assert.AreEqual("This is a test.", Parser.Parse(" \"This is a test.\" "), "Quoted string not recognized");
+            Assert.AreEqual("  \"/\\\b\f\n\r\t\u263a\\x  ", Parser.Parse(" \"  \\\"\\/\\\\\\b\\f\\n\\r\\t\\u263A\\x  \" "), "JSON string escape sequences not recognized");
 
+            // Test objects and arrays.
             // CollectionAssert.AreEqual() does not work for sub-collections; Check manually.
             object obj = Parser.Parse("{ \"key1\" : true , \"key2\" : [ 1, 2, 3, 4, 5], \"key3\" : { \"k1\": \"test1\", k2:\"test2\"}}");
             Dictionary<string, object> obj_dict = (Dictionary<string, object>)obj;
-            Assert.AreEqual(3, obj_dict.Count);
-            Assert.AreEqual(true, obj_dict["key1"]);
+            Assert.AreEqual(3, obj_dict.Count, "Incorrect number of child elements");
+            Assert.AreEqual(true, obj_dict["key1"], "Child element mismatch");
             CollectionAssert.AreEqual(new List<object>
                 {
                     1,
@@ -51,12 +53,12 @@ namespace eduJSON.Tests
                     3,
                     4,
                     5
-                }, (List<object>)obj_dict["key2"]);
+                }, (List<object>)obj_dict["key2"], "Child element mismatch");
             CollectionAssert.AreEqual(new Dictionary<string, object>
                 {
                     { "k1", "test1" },
                     { "k2", "test2" }
-                }, (Dictionary<string, object>)obj_dict["key3"]);
-        }
+                }, (Dictionary<string, object>)obj_dict["key3"], "Child element mismatch");
+       }
     }
 }
