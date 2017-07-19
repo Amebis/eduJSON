61e3b9a3907ab23b697f46ba0a5b223b16d83258
Simon Rozman <simon@rozman.si>
2017-07-19 10:19:22 +0200

    eduJSONTests >> eduJSON.Tests

--- eduJSON.Tests/Properties/AssemblyInfo.cs
+++ eduJSON.Tests/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("eduJSONTests")]
+[assembly: AssemblyTitle("eduJSON.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Amebis")]
